c4326e916479152a84adb626410faebb6ac4d81a
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-08-19 16:01:32 -0300

    feat: adicionando comportamento de hover nos posts

--- src/pages/Blog/components/PostCard/style.ts
+++ src/pages/Blog/components/PostCard/style.ts
@@ -32,4 +32,15 @@ export const PostCardContainer = styled.div`
 
     color: ${(props) => props.theme['base-text']};
   }
+
+  &:hover {
+    opacity: 0.9;
+    box-shadow:
+      rgba(0, 0, 0, 0.25) 0px 54px 55px,
+      rgba(0, 0, 0, 0.12) 0px -12px 30px,
+      rgba(0, 0, 0, 0.12) 0px 4px 6px,
+      rgba(0, 0, 0, 0.17) 0px 12px 13px,
+      rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    cursor: pointer;
+  }
 `
