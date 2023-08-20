ae44395f392d9a64170f6b281db10ff4a1d29b45
atilio ferreira <atilio.silvaferreira@gmail.com>
2023-08-20 09:43:58 -0300

    feat: configuring react hook form

--- src/pages/Blog/components/SearchForm/index.tsx
+++ src/pages/Blog/components/SearchForm/index.tsx
@@ -1,13 +1,28 @@
+import { useForm } from 'react-hook-form'
 import { SearchFormContainer } from './style'
 
+interface SearchFormData {
+  postName: string
+}
+
 export function SearchForm() {
+  const { register, handleSubmit } = useForm<SearchFormData>()
+
+  function onSubmit(data: SearchFormData) {
+    console.log(data)
+  }
+
   return (
-    <SearchFormContainer>
+    <SearchFormContainer onSubmit={handleSubmit(onSubmit)}>
       <div>
         <h2>Publicações</h2>
         <span>6 publicações</span>
       </div>
-      <input type="text" placeholder="Buscar conteúdo" />
+      <input
+        type="text"
+        placeholder="Buscar conteúdo"
+        {...register('postName')}
+      />
     </SearchFormContainer>
   )
 }
